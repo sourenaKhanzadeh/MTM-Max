bc8abddb5c9a92644f340df4c871838c463a8b6b
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-11-26 22:08:30 -0500

    Sign Up Page created.....

--- Movie.py
+++ Movie.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from database import cursor,db
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('homepage.html')
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == "GET":
+        return render_template('homepage.html')
+    else:
+        return "Hello"
 
+@app.route('/create/<member>')
+def create_member(member):
+    return render_template('sign_up.html', mem=member)
 
 if __name__ == '__main__':
     app.run(debug=True)
