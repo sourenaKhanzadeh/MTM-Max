5ed106fbfc0234c885b6e4c0d6076e41d0bdb977
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-11-26 20:41:56 -0500

    Tables  created.....

--- database.py
+++ database.py
@@ -29,5 +29,91 @@ if len(tables) == 0:
         Username VARCHAR(30) NOT NULL
     )
 """)
-    
+
+    cursor.execute("CREATE TABLE admin (AdminID INTEGER PRIMARY KEY REFERENCES movie_user(UserID))")
+
+    cursor.execute("CREATE TABLE customer (CustomerID INTEGER PRIMARY KEY REFERENCES movie_user(UserID))")
+
+    cursor.execute("""
+    CREATE TABLE product (
+        ProductID INTEGER PRIMARY KEY,
+        Name VARCHAR(50) NOT NULL,
+        Length INTEGER NOT NULL,
+        Description VARCHAR(280),
+        Genre VARCHAR(50),
+        SellPrice FLOAT NOT NULL,
+        MaturityRating VARCHAR(10),
+        RottenTomatoRating VARCHAR(10),
+        ReleaseDate  DATE
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE movie (
+        MovieID INTEGER PRIMARY KEY REFERENCES product(ProductID),
+        Movie_Cast VARCHAR(100),
+        Languages VARCHAR(100),
+        Director VARCHAR(50),
+        Country VARCHAR(50)	
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE music (
+      SongID INTEGER PRIMARY KEY REFERENCES product(ProductID),
+      ArtistName VARCHAR(100) NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE tvseries (
+        SeriesID INTEGER PRIMARY KEY REFERENCES product(ProductID),
+        Seasons INTEGER NOT NULL,
+        Episodes INTEGER NOT NULL,
+        TV_Cast VARCHAR (100)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE seasons (
+        SeriesID INTEGER REFERENCES product(ProductID),
+        SeasonNumber INTEGER NOT NULL,
+        NumberOfEpisodes INTEGER NOT NULL,
+        PRIMARY KEY(SeriesID, SeasonNumber)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE subscription (
+        Type VARCHAR(20) PRIMARY KEY,
+        Price INTEGER NOT NULL,
+        TVSeriesPerMonth INTEGER NOT NULL,
+        MoviesPerMonth INTEGER NOT NULL,
+        MusicPerMonth INTEGER NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE streams (
+        CustomerID INTEGER NOT NULL REFERENCES customer(CustomerID),
+        ProductID INTEGER NOT NULL REFERENCES product(ProductID)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE manages (
+        AdminID INTEGER NOT NULL REFERENCES admin(AdminID),
+        CustomerID INTEGER REFERENCES customer(CustomerID),
+        ProductID INTEGER REFERENCES product(ProductID)
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE purchases (
+        CustomerID INTEGER NOT NULL REFERENCES customer(CustomerID),
+        SubscriptionType VARCHAR(20) REFERENCES subscription(Type),
+        ProductID INTEGER REFERENCES product(ProductID),
+        DateOfPurchase DATE
+    )
+    """)
     print("Tables Created...")
